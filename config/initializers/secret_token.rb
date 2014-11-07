b4dd811291b6720d3ff4ed03bf51422f7e905692
Alex Notov <anotov@gmail.com>
2014-11-06 23:58:53 -0800

    Initial commit after config steps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BartSchedules::Application.config.secret_key_base = 'ec80d1d9939c708ba38679af90ee2c3c995b6e894105aa79c5fcef336a0f3fa6e25d5a605b8355f41ca2442f85708c11a4a60394c174c48ca914070d03223363'
