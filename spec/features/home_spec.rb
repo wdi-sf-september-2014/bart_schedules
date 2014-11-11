b6150c62ef2aa84cf33717a89ad32ee5c1259741
Alex Notov <anotov@gmail.com>
2014-11-10 16:01:37 -0800

    Add comments to features/home_spec.rb

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 feature "Home", :type => :feature do
   scenario "visiting the home page" do
     # create a new station, based on the definition
-    # of the station factory
+    # of the station factory in spec/factories/stations.rb
     station = create(:station)
 
+    # see https://github.com/jnicklas/capybara#navigating
     visit root_path    
 
+    # see https://github.com/jnicklas/capybara#querying
     expect(page).to have_text("Departures by station")
     expect(page).to have_text(station.name)
   end
